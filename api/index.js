9dc563689fd0099dfedcca1ddb20c26fd378e401
TaeDaDev <uhsaantae@gmail.com>
2026-05-14 23:07:52 -0400

    add vercel config and api serverless function

--- api/index.js
+++ api/index.js
@@ -93,5 +93,5 @@ app.delete("/jobs/:id", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+export default app; 
